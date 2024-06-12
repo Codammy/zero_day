e626d0b56283427f363f50229a5d31be02a068a7
Codam <damisco005@gmail.com>
2024-06-12 05:40:46 -0700

    Added python script

---
+++ index.py
@@ -0,0 +1,14 @@
+""" Grade calculator """
+
+score = 140
+
+if score > 75 and score <= 100:
+    print("A")
+elif score >=60 and score < 75:
+    print("B")
+elif score >= 50 and score < 60:
+    print("C")
+elif score >= 45 and score < 50:
+    print("D")
+else:
+    print("F")
